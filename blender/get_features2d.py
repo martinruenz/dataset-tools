cde53dda67f787e1a022c68929c48effe220be2e
JingwenWang95 <Jingwen.Wang.17@ucl.ac.uk>
2020-04-03 09:55:33 +0100

    changed some code to adapt to blender 2.82, added code for recording camera poses

--- blender/get_features2d.py
+++ blender/get_features2d.py
@@ -17,14 +17,18 @@
 
 import bpy
 from bpy_extras.object_utils import world_to_camera_view
+from math import pi
+from mathutils import Euler
+
 
 scene = bpy.context.scene
 cam = scene.camera
-mesh = bpy.data.objects['blok_6'] # TODO change object here
+mesh = bpy.data.objects['spraybottle'] # TODO change object here
+# get depandency map
+degp = bpy.context.evaluated_depsgraph_get()
 
-tom = mesh.matrix_world.inverted()
-
-file=open(bpy.path.abspath("//tracks.txt"),'w')
+fg_file = open(bpy.path.abspath("//object_features_2d.txt"),'w')
+poses_file = open(bpy.path.abspath("//poses.txt"), 'w')
 
 for f in range(bpy.context.scene.frame_start, bpy.context.scene.frame_end):
 
@@ -35,10 +39,31 @@ for f in range(bpy.context.scene.frame_start, bpy.context.scene.frame_end):
   num_frustrum = 0
   num_bad = 0
 
+  # object pose Tow
+  T_ow = mesh.matrix_world.inverted()
+  # camera pose under blender convention (z pointing backward)
+  R_wc_bpy = cam.matrix_world.to_3x3()
+  R_bpy_to_slam = Euler((pi, 0, 0), "XYZ").to_matrix()
+  R_wc_slam = R_wc_bpy @ R_bpy_to_slam
+  T_wc_slam = R_wc_slam.to_4x4()
+  t_wc = cam.matrix_world.to_translation()
+  T_wc_slam.translation = t_wc
+  
+  q_wo = mesh.matrix_world.to_quaternion()
+  t_wo = mesh.matrix_world.to_quaternion()
+  q_wc = T_wc_slam.to_quaternion()
+  poses_file.write("%f %f %f %f %f %f %f " % (t_wc.x, t_wc.y, t_wc.z, q_wc.x, q_wc.y, q_wc.z, q_wc.w))
+  poses_file.write("%f %f %f %f %f %f %f " % (t_wo.x, t_wo.y, t_wo.z, q_wo.x, q_wo.y, q_wo.z, q_wo.w))
+  poses_file.write("\n")
+  
+  # evaluate depandency map
+  particle_systems = mesh.evaluated_get(degp).particle_systems
+  
+  # camera position
   cam_t = cam.matrix_world.to_translation()
-  cam_tm = tom * cam_t
+  cam_tm = T_ow @ cam_t
 
-  for p in mesh.particle_systems[0].particles:
+  for p in particle_systems[0].particles:
     ID = ID + 1
 
     # Check if point falls into cam frustrum
@@ -48,7 +73,8 @@ for f in range(bpy.context.scene.frame_start, bpy.context.scene.frame_end):
     num_frustrum = num_frustrum + 1
 
     # Intersect
-    feature_tm = tom * p.location
+    # feature coordinate under object coordinate frame
+    feature_tm = T_ow @ p.location
     d = feature_tm - cam_tm
     d.normalize()
     hit, intersection_tm, intersection_n, intersection_index = mesh.ray_cast(cam_tm, d)
@@ -67,11 +93,12 @@ for f in range(bpy.context.scene.frame_start, bpy.context.scene.frame_end):
     px = int(proj.x * scene.render.resolution_x * render_scale)
     py = int(scene.render.resolution_y * render_scale) - int(proj.y * scene.render.resolution_y * render_scale)
 
-    file.write('%d %d %d ' %(ID, px, py))
+    fg_file.write('%d %d %d ' %(ID, px, py))
 
   print("There were ", num_frustrum, " features inside the frustrum, of which ",
           num_visible, " were visible in frame (",num_bad," bad casts) ...")
 
-  file.write('\n')
+  fg_file.write('\n')
 
-file.close()
+fg_file.close()
+poses_file.close()
